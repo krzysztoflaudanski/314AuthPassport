74e3618c87dd3e7980c5d744bceeb8dfe4d0a575
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-11-21 11:05:26 +0100

    fix endpoints user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,23 +6,23 @@ function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-    res.redirect('/no-permission');
+    res.redirect('/user/no-permission');
 }
 
-router.get('/logged', (req, res) => {
+router.get('/user/logged', ensureAuthenticated, (req, res, next) => {
     res.render('logged', { displayName: req.user.displayName, picture: req.user._json.picture });
 });
 
-router.get('/no-permission', (req, res) => {
+router.get('/user/no-permission', (req, res) => {
     res.render('noPermission');
 });
 
 // Trasy dostępne tylko dla zalogowanych użytkowników
-router.get('/profile/', ensureAuthenticated, (req, res) => {
+router.get('/user/profile/', ensureAuthenticated, (req, res) => {
     res.render('profile', { user: req.user });
 });
 
-router.get('/profile/settings', ensureAuthenticated, (req, res) => {
+router.get('/user/profile/settings', ensureAuthenticated, (req, res) => {
     res.render('settings', { user: req.user });
 });
 
